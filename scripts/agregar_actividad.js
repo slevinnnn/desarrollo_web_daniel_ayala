b2530623086b9758dbf4cb6a73d9e5e7804981a5
slevinnnn <danielbm3012@gmail.com>
2025-04-02 00:59:57 -0300

    se termino de implementar la modalidad para ir moviendose conforme se va completando el formulario de agregar actividad.se agrego la funcion para cargar la data de regiones y comuna y se comenzo a escribir la funcion para poblar el select de region

--- scripts/agregar_actividad.js
+++ scripts/agregar_actividad.js
@@ -1,10 +1,32 @@
+async function obtener_regiones_y_comunas() {
+  try {
+      const response = await fetch('region_comuna.json'); // Carga el archivo JSON
+      if (!response.ok) throw new Error('Error al cargar el JSON'); // Manejo de errores
+      const data = await response.json(); // Convierte la respuesta en un objeto JS
+      console.log(data.regiones[0].nombre);
+      console.log(data.regiones[1].id);   // 25
+  } catch (error) {
+      console.error(error);
+  }
+}
+
+// Llamar a la función
+obtener_regiones_y_comunas();
+
+
 let contador=0;
 const  nextStep = (step) => {
   console.log(contador)
   console.log(step)
     if (step >= 3) return;
-    contador++;
+    contador=step;
     document.getElementById("contenedor_formulario").style.transform = `translateX(-${contador * 33.33}%)`;
+}
+const  backStep = (step) => {
+  console.log(contador)
+  console.log(step)
+    if (step <= 1) return;
+    document.getElementById("contenedor_formulario").style.transform = `translateX(-${(step-2)* 33.33}%)`;
 }
 
 document.querySelectorAll(".btn_siguiente").forEach((button) => {
@@ -14,3 +36,23 @@ document.querySelectorAll(".btn_siguiente").forEach((button) => {
   });
 }
 );
+document.querySelectorAll(".btn_atras").forEach((button) => {
+  button.addEventListener("click", () => {
+    let button_id=Number(button.id)
+    backStep(button_id);
+  });
+}
+);
+
+const poblarRegionComuna = () => {
+  let regionSelect = document.getElementById("select_region");
+
+
+
+
+}
+
+
+
+
+
