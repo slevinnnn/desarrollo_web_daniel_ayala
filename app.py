55887d6221f66defbaa32f7668baf5ddffa4e896
slevinnnn <danielbm3012@gmail.com>
2025-05-05 01:15:31 -0400

    se movieron las carpetas estaticas dentro de la carpeta static y se corrigieron las redirecciones del html index de imagenes a static/

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, request, render_template, redirect, url_for, session
+from utils.validations import validate_login_user, validate_register_user, validate_confession
+from database import db
+from werkzeug.utils import secure_filename
+import hashlib
+import filetype
+import os
+
+UPLOAD_FOLDER = 'static/uploads'
+
+app = Flask(__name__)
+
+app.secret_key = "s3cr3t_k3y"
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024  # 16 MB
+app.config['ALLOWED_EXTENSIONS'] = {'png', 'jpg', 'jpeg', 'gif'}
+
+
+# --- Routes ---
+@app.route("/")
+def index():
+    
+    return render_template("index.html")
